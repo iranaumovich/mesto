0f5ee59bf56db6b63fe870b2fa4f1f2b2334c9e4
Iryna Tsupryk <iranaumovich0@gmail.com>
2022-12-23 22:16:51 +0100

    fix: исправить js после 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -7,23 +7,28 @@ const formElement = document.querySelector('.form');
 const nameInput = formElement.querySelector('.form__item_type_name');
 const descriptionInput = formElement.querySelector('.form__item_type_description');
 
+function openPopup() {
+  popUp.classList.add('popup_opened');
+  nameInput.value = userName.textContent;
+  descriptionInput.value = userDescription.textContent;
+}
+
+function closePopup() {
+  popUp.classList.remove('popup_opened');
+}
+
 function handleFormSubmit(evt) {
   evt.preventDefault();
-
   userName.textContent = nameInput.value;
   userDescription.textContent = descriptionInput.value;
-  popUp.classList.toggle('popup_opened');
+  closePopup();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
 
-editButton.addEventListener('click', function () {
-  popUp.classList.add('popup_opened');
-});
+editButton.addEventListener('click', openPopup);
 
-closeButton.addEventListener('click', function () {
-  popUp.classList.remove('popup_opened');
-});
+closeButton.addEventListener('click', closePopup);
 
 
 
